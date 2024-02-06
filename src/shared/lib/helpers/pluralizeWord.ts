eff9c566709b46b5bbd897614d8d321a57ad43cf
Maxim Zudin <mzudin.reon@gmail.com>
2024-02-06 14:29:18 +0300

    fix: couple fixes

--- src/shared/lib/helpers/pluralizeWord.ts
+++ src/shared/lib/helpers/pluralizeWord.ts
@@ -1,5 +1,4 @@
 export const pluralizeWord = (count: number) => {
-	// Определяем правильное склонение в зависимости от числа
 	if (count === 1) {
 	  return 'товар';
 	} else if (count % 10 >= 2 && count % 10 <= 4 && (count % 100 < 10 || count % 100 >= 20)) {
