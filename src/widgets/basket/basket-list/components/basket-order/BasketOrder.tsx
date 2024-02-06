04ca282c5ce726f09c55dfe54233432980849c93
Maxim Zudin <mzudin.reon@gmail.com>
2024-02-06 15:01:34 +0300

    fix: couple fixes

--- src/widgets/basket/basket-list/components/basket-order/BasketOrder.tsx
+++ src/widgets/basket/basket-list/components/basket-order/BasketOrder.tsx
@@ -5,24 +5,30 @@ import { useAppSelector } from 'src/shared/model/store';
 import { pluralizeWord } from 'src/shared/lib/helpers';
 
 interface BasketOrderProps {
-	className?: string
-	style?: CSSProperties
+	className?: string;
+	style?: CSSProperties;
 }
 
-export const BasketOrder:FC<BasketOrderProps> = () => {
-	const basketGoodsCount = useAppSelector(store => store.basket.basket.length);
+export const BasketOrder: FC<BasketOrderProps> = () => {
+	const basketGoodsCount = useAppSelector(
+		store => store.basket.basket.length
+	);
 	const basketGoodsAmount = useAppSelector(store => {
-		return store.basket.basket.reduce((acc, curVal) => acc + curVal.price, 0)
+		return store.basket.basket.reduce(
+			(acc, curVal) => acc + curVal.price,
+			0
+		);
 	});
-	
+
 	const placeAnOrder = useCallback(() => {
 		alert(`Вы оформили заказ на сумму ${basketGoodsAmount}, поздравляю`);
-	}, [])
+	}, []);
 
 	return (
 		<div className={cl['basket-order']}>
 			<div className={cl['basket-order__info']}>
-				Итого: { basketGoodsCount } { pluralizeWord(basketGoodsCount) } на сумму { basketGoodsAmount }&#8381;
+				Итого: {basketGoodsCount} {pluralizeWord(basketGoodsCount)} на
+				сумму {basketGoodsAmount}&#8381;
 			</div>
 			<div className={cl['basket-order__ctrl']}>
 				<Button
